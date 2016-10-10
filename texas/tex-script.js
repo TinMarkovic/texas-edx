34110ac015f8d8db20e46f2def6d7754a3223238
Tin <tinmarkoviccs@gmail.com>
2016-10-10 14:34:06 +0200

    Loading LTI blocks through the API, links are still hardcoded, replacing them is TBI

--- texas/tex-script.js
+++ texas/tex-script.js
@@ -1,7 +1,6 @@
 var button = [];
 var LTIblocks = [];
 window.onload = function() {
-  // zasad upeceno
   var apiBlocksFromCourseRoute = "/api/courses/v1/blocks/?course_id="
   var apiUsernameSetting = "&username=";
   var apiDepthSetting = "&depth=all";
@@ -32,18 +31,14 @@ window.onload = function() {
           'width':'1',
           'src': LTIblocks[i].student_view_url
       }).appendTo('#lti-iframes-hidden-container');
+
+      // TODO: Module button generation
+
     }
   });
-  // Need to find a better way for controlling this:
-  /*if (window.location.port){
-    $("#lti-iframes-hidden-container").html("");
-  } else {
-    $("#lti-iframes-hidden-container").html('<iframe id="financial-aid-advising" style="border:none;" height="1" width="1" src="http://microsite.tex.extensionengine.com/xblock/block-v1:TexasOnCourse+TXOC101+2016_T2+type@lti_consumer+block@788c479d8370471185c422ffbd198a82"> </iframe><iframe id="career-advising" style="border:none;" height="1" width="1" src="http://microsite.tex.extensionengine.com/xblock/block-v1:TexasOnCourse+TXOC101+2016_T2+type@lti_consumer+block@4932fabbde5445b0a5eb29ccb5947453"> </iframe><iframe id="postsecondary-advising" style="border:none;" height="1" width="1" src="http://microsite.tex.extensionengine.com/xblock/block-v1:TexasOnCourse+TXOC101+2016_T2+type@lti_consumer+block@cc3b50c9ed2d441893d68a66aa2c9689"> </iframe>');
-  }*/
 };
 
 $(".lo-mainclass a").on("click", function(event) {
-  // Probably should look if they're loaded already, but for now it's working.
   loadButtons();
   button[event.currentTarget.id].click();
 });
